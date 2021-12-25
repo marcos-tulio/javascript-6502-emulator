5855a5353661db1cc5f0f14cc69f6b0d84d95ed5
Marcos Santos <marcos.ts201128@gmail.com>
2021-12-25 19:27:55 -0300

    separado a view em uma nova classe e adicionada classe para o emulador

--- js/emulator.js
+++ js/emulator.js
@@ -1,2 +1,65 @@
-const CPU = new Cpu()
-const BUS = new Bus(CPU)
+//-------------------------------------------------------------
+//                          Emulator
+//-------------------------------------------------------------
+class Emulator{
+
+    constructor(){
+        this.cpu = new Cpu()
+        this.bus = new Bus(this.cpu)
+        this.screen = new Screen(this.cpu)   
+        
+        // Source
+        this.source_offset = 0x8000
+        this.source = [
+            0xA2, 0x0A, 0x8E, 0x00, 0x00, 0xA2, 0x03, 0x8E, 0x01, 0x00, 
+            0xAC, 0x00, 0x00, 0xA9, 0x00, 0x18, 0x6D, 0x01, 0x00, 0x88, 
+            0xD0, 0xFA, 0x8D, 0x02, 0x00, 0xEA, 0xEA, 0xEA
+        ]
+    }
+
+    init(){
+        // clear ram
+        for (const i in this.bus.ram)
+            this.bus.ram[i] = 0x00
+
+        // load code to ram            
+        let offset = this.source_offset
+        for (const b of this.source) 
+            this.bus.ram[offset++] = b
+
+        // reset vectors
+        this.bus.ram[0xFFFC] = 0x00;
+        this.bus.ram[0xFFFD] = 0x80;
+
+        // reset
+        this.cpu.reset()
+        this.screen.init()
+    }
+
+    runAuto(btn, is_call_back = false, context = this){
+        if (!is_call_back){
+            btn.textContent = (btn.textContent == "Run") ? "Stop" : "Run"
+        }
+        
+        if (btn.textContent == "Stop"){
+            context.runManual()
+            setTimeout(context.runAuto, document.getElementById("interval").value, btn, true, context)
+        }
+    }
+
+    runManual(){
+        do this.cpu.clock()
+        while (!this.cpu.complete())
+
+        this.screen.refresh()
+    }
+
+    async loadRom(file){
+        const array_buffer = await (new Response(file)).arrayBuffer()
+        this.source = new Uint8Array(array_buffer)
+
+        this.screen.init()
+    }
+}
+
+
---
+++ js/screen.js
@@ -0,0 +1,114 @@
+//-------------------------------------------------------------
+//                         Screen
+//-------------------------------------------------------------
+class Screen{
+
+    constructor(cpu){
+        this.cpu = cpu
+
+        // Table config.
+        this.size_x = 25
+        this.size_y = 20
+        this.title_size = 40
+        this.padding = 10
+
+        // ASM Map
+        this.map_asm = []
+    }
+
+    init(){
+        // init asm mapper 
+        this.map_asm = this.cpu.disassemble(0x0000, 0xFFFF)            
+
+        // Canvas config.
+        const lstCanvas = document.getElementsByTagName("canvas");
+        for (let i = 0; i < lstCanvas.length; i++)
+            lstCanvas[i].getContext("2d").font = "12px monospace"
+
+        this.refresh()
+    }
+
+    getCanvasClearned(id){
+        const canvas = document.getElementById(id)
+        const context = canvas.getContext("2d")
+
+        context.clearRect(0, 0, canvas.width, canvas.height);
+
+        return canvas
+    }
+
+    updateRam(id, address, cols = 16, rows = 16){
+        const ctx = this.getCanvasClearned(id).getContext("2d")
+
+        let row = 0
+        let col = 0            
+        for (let i = address; i < this.cpu.bus.ram.length; i++){
+            if (i % cols == 0){
+                col = 0
+                row++
+
+                if (row > rows) break;
+            }
+
+            if (col == 0)
+                ctx.fillText("$" + toHex(i, 4), col++ * this.size_x + this.padding, row * this.size_y)
+            
+            ctx.fillText(toHex(this.cpu.bus.ram[i]), col++ * this.size_x + this.title_size, row * this.size_y)  
+        }  
+    }
+
+    updateCpu(){
+        const ctx = this.getCanvasClearned("cpu").getContext("2d")
+        
+        let flags = Object.entries(this.cpu.FLAG)
+        for (let i = 0; i < flags.length; i++) {
+            ctx.fillStyle = (flags[i][1] & this.cpu.status) > 0 ? "green" : "red"
+            ctx.fillText(flags[i][0], (i) * this.size_x + this.padding, this.size_y)
+        }
+
+
+        // Registers
+        ctx.fillStyle = "black"
+        ctx.fillText("PC: $" + toHex(this.cpu.pcount, 4), this.padding, this.size_y * 2)
+        ctx.fillText("SP: $" + toHex(this.cpu.stack, 2), this.padding, this.size_y * 3)
+        ctx.fillText(" A: $" + toHex(this.cpu.acc, 2)   + " ("+ this.cpu.acc   + ")", this.padding, this.size_y * 4)
+        ctx.fillText(" X: $" + toHex(this.cpu.reg_x, 2) + " ("+ this.cpu.reg_x + ")", this.padding, this.size_y * 5)
+        ctx.fillText(" Y: $" + toHex(this.cpu.reg_y, 2) + " ("+ this.cpu.reg_y + ")", this.padding, this.size_y * 6)
+    }
+
+    updateDisassemble(lines = 10){
+        const ctx = this.getCanvasClearned("debugger").getContext("2d")
+
+        const offsets = []
+        let offset_selected = undefined
+        let down_lines = parseInt(lines / 2);
+
+        // down check
+        for (let i = this.cpu.pcount; i >= 0; i--) {
+            if (this.map_asm[i]){
+                offsets.push(i)
+                if (!offset_selected) offset_selected = i
+                if (!down_lines--) break
+            }
+        }
+
+        // up check
+        for (let i = this.cpu.pcount + 1; i < this.cpu.bus.ram.length; i++) {
+            if (this.map_asm[i]){
+                offsets.push(i)
+                if (offsets.length == lines) break
+            }
+        }
+
+        // draw codes
+        let line = 1
+        for (const _offset of offsets.sort( function(a,b) { return a - b; } )) {
+            ctx.fillStyle = (_offset == offset_selected) ? "green" : "black"
+            ctx.fillText(this.map_asm[_offset], this.padding, line++ * this.size_y)
+        }
+    }
+
+    // This function needs can be overrided
+    refresh(){}
+
+}
